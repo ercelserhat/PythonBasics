1df3a593e38b600885ba3c1fe8453a5d06c9aa97
ercelserhat <yr503941@yargitay.gov.tr>
2022-11-04 15:04:04 +0300

    Escape sequences

--- PrintFunction.py
+++ PrintFunction.py
@@ -20,3 +20,31 @@ print("Python", "Programlama", "Dili", 2022, file = dosya)
 dosya.close()
 
 print(*"Python", sep = "-")
+
+
+#ESCAPE SEQUENCES
+
+#  \’ Karakter dizisi içinde tek tırnak işaretini kullanabilmemizi sağlar.
+#  \” Karakter dizisi içinde çift tırnak işaretini kullanabilmemizi sağlar.
+#  \\ Karakter dizisi içinde \ işaretini kullanabilmemizi sağlar.
+#  \n Yeni bir satıra geçmemizi sağlar.
+#  \t Karakterler arasında sekme boşluğu bırakmamızı sağlar.
+#  \u UNICODE kod konumlarını gösterebilmemizi sağlar.
+#  \U UNICODE kod konumlarını gösterebilmemizi sağlar.
+#  \N Karakterleri UNICODE adlarına göre kullanabilmemizi sağlar.
+#  \x Onaltılı sistemdeki bir sayının karakter karşılığını gösterebilmemizi sağlar.
+#  \a Destekleyen sistemlerde, kasa hoparlöründen bir ‘bip’ sesi verilmesini sağlar.
+#  \r Aynı satırın başına dönülmesini sağlar.
+#  \v Destekleyen sistemlerde düşey sekme oluşturulmasını sağlar.
+#  \b İmlecin sola doğru kaydırılmasını sağlar
+#  \f Yeni bir sayfaya geçilmesini sağlar.
+#  r Karakter dizisi içinde kaçış dizilerini kullanabilmemizi sağlar.,
+
+print('Ankara\'nın havası')
+print("Satır 1\nSatır 2\nSatır 3") 
+print("Bir\tİki\tÜç") 
+print("\a") 
+print("Merhaba \rDünya") 
+print("Merhaba \vDünya") 
+print("yahoo.com\b.uk") 
+print(r"C:\Program Files\Adobe") 
