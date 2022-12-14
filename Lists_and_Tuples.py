d3caa244359b654f46521d874a168de2dfcfafde
Serhat ERÇEL <ercelserhat@gmail.com>
2022-12-14 17:30:11 +0300

    List Comprehensions

--- Lists_and_Tuples.py
+++ Lists_and_Tuples.py
@@ -94,3 +94,26 @@ print(liste10) #['veli', 'mehmet', 'özlem']
 
 liste11 = list(liste9)
 print(liste11) #['ahmet', 'mehmet', 'özlem']
+
+
+#Liste Üreteçleri (List Comprehensions)
+liste12 = [i for i in range(100)]
+print(liste12)
+
+liste13 = list(range(100))
+print(liste13)
+
+listeCiftSayilar = [i for i in range(100) if i % 2 == 0]
+print(listeCiftSayilar)
+#[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 
+#52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98]
+
+liste14 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+liste15 = []
+for i in liste14:
+    for x in i:
+        liste15 += [x]
+print(liste15) #[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+liste16 = [x for i in liste14 for x in i]
+print(liste16)
